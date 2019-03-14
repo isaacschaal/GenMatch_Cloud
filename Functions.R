9366e85aabc2c832455b6319b2a1d10cd11e58b4
isaacschaal <33569747+isaacschaal@users.noreply.github.com>
2019-03-14 13:45:28 -0300

    Update Functions.R
    Updated the function to the working version

--- Functions.R
+++ Functions.R
@@ -24,19 +24,21 @@ library(rgenoud)
 
 #####
 
-#extractDataFromGenoud <- function(x, balancematrix, pop, y, treatment, df){
 extractDataFromGenoud <- function(X, BalanceMatrix, Y, Tr, df, cl, balance_formul, pop){
-
+  
   # Create temp directory for the genoud.pro
   temp <- toString(tempdir())
   
   # Do genout, mout and mb for one set
   genout <- GenMatch(Tr=Tr, X=X, BalanceMatrix=BalanceMatrix, estimand="ATT", M=1,
-                     pop.size=100, max.generations=20, wait.generations=5, cluster = cl, print.level = 3,
+                     pop.size=25, max.generations=10, wait.generations=5, cluster = cl, print.level = 3,
                      project.path = cat(temp,"genoud.pro"), nboots = 500)
-        # print level >3 means it prints all gens, and file is made
-        # path is the temp dir where genoud.pro goes
+  # print level >3 means it prints all gens, and file is made
+  # path is the temp dir where genoud.pro goes
   
+  # genout <- GenMatch(Tr=Tr, X=X, BalanceMatrix = BalanceMatrix,
+  #                    estimand="ATT",
+  #                    pop.size=10, max.generations=10, wait.generations=5, cluster = cl)
   
   mout <- Match(Y=Y, Tr=Tr, X=X, estimand="ATT", Weight.matrix=genout)
   summary(mout)
@@ -47,15 +49,15 @@ extractDataFromGenoud <- function(X, BalanceMatrix, Y, Tr, df, cl, balance_formu
   # Overwrite the previous genoud, write the full output of genout
   # also, is taking genoud.pro and turning to genoud.txt
   file.copy(paste(gsub("//", "/", temp),"/genoud.pro", sep = ""),"./genoud.txt",overwrite = TRUE)
-
+  
   # load the file, named as con
   con <- file("genoud.txt", "r")
-
+  
   # output reads the lines of the file
   output <- readLines(con)
-
+  
   #### Output looks like this
-
+  
   # first line is "Generation: 0 \t Population Size: 20 \t Fit Values: 22 \t Variables: 10"
   # then ""
   # then 1-20 (b/c one for each member of pop)
@@ -63,9 +65,9 @@ extractDataFromGenoud <- function(X, BalanceMatrix, Y, Tr, df, cl, balance_formu
   # first 22 are the $value of genout, then the last 10 are the weights
   # this repeats for each child
   # then repeat for each generation
-
+  
   ####
-
+  
   ## drops all of the rows that are spaces or the generation
   for(row in output){
     if(substr(row,1,1) == "G" || substr(row,1,1) == ""){
@@ -73,50 +75,51 @@ extractDataFromGenoud <- function(X, BalanceMatrix, Y, Tr, df, cl, balance_formu
       output <- output[-index]
     }
   }
-
+  
   # delets all the tabs
   output <- gsub(" \t ", ",",output)
   # then gets a list for of each line
   outputlist <- strsplit(output,"[,]")
-
+  
   # create a new dataframe
   # The first col will be the lowest p val, the last col will be the treatment effect
   # and the middle cols will be the weights of the weight matrix
-  cols = ncol(BalanceMatrix)
-  df <- matrix(data = NA, ncol = cols+1, nrow = length(outputlist))
-      #This was updated (ncol should be 1 more than cols)
+  cols = ncol(X)
+  df <- matrix(data = NA, ncol = cols+2, nrow = length(outputlist)) # changed
+  #This was updated (ncol should be 1 more than cols)
+  end <- length(outputlist[[1]])
+  start <- end - cols+1
   
   for(candidate in c(1:length(outputlist))){
-
+    
     # 1st # is the member of the pop
     # 2nd number is the lowest p val ( the next 21 are the other p vals (2 for each) )
     # 23rd is the 1st of weights, till 32
-
+    
     # Get the lowest p val and the weight matrix
-    selected <- outputlist[[candidate]][c(2,(cols*2+2):(cols*2+ncol(X)+1))]
-
+    
+    selected <- outputlist[[candidate]][c(2,start:end)]
+    
     # Convert them to numeric
     selection <- as.numeric(selected)
-
+    
     # set the first 11 to what we have selected
-    df[candidate,c(1:cols)] <- selection
+    for_sel = cols +1
+    df[candidate,c(1:for_sel)] <- selection
   }
-
+  
   for(i in c(1:nrow(df))){
     # matching using the weight matrix
-                                                     # diag makes the list turn into diag of matrix
-    mout <- Match(Y=Y, Tr=Tr, X=X, estimand="ATT", Weight.matrix=diag(df[i,c(2:(ncol(df)-1))]))
+    # diag makes the list turn into diag of matrix
+    mout <- Match(Y=Y, Tr=Tr, X=X, estimand="ATT", Weight.matrix=diag(df[i,c(2:(ncol(df)-1))])) # its here i think
     # changed the y and x and treat
     ### CHANGE TO have the correct inputs
-
+    
     # put it in the last col
     df[i,ncol(df)] <- mout$est
-
+    
   }
   return (df)
-  
-
-  #return(df)
 }
 
 #####
